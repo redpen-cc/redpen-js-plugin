8bfd8f7ee0d84b23e0f0f9976046f87c499e4350
takahi-i <takahiko03@gmail.com>
2017-09-24 20:12:49 +0900

    Correct indentation

--- test/validator-test.js
+++ test/validator-test.js
@@ -8,12 +8,12 @@ before(function() {
 describe("validator", function() {
 	it('test validator.js mod', function () {
 		let errorSentences = redpen.callRedPen({"document" : "This sentence contains toolongword. This sentence do not contain too long word."});
-    assert.equal(errorSentences.length, 1);
-    firstErrorSentence = errorSentences[0];
-	  assert.equal(firstErrorSentence.sentence, 'This sentence contains toolongword.');
-	  // there is one too word exceeds 10 chalacteres long
-	  assert.equal(1, firstErrorSentence.errors.length);
-	  assert.equal('word [toolongword.] is too long. length: 12', firstErrorSentence.errors[0].message);
+		assert.equal(errorSentences.length, 1);
+		firstErrorSentence = errorSentences[0];
+		assert.equal(firstErrorSentence.sentence, 'This sentence contains toolongword.');
+		// there is one too word exceeds 10 chalacteres long
+		assert.equal(1, firstErrorSentence.errors.length);
+		assert.equal('word [toolongword.] is too long. length: 12', firstErrorSentence.errors[0].message);
 	});
 });
 
